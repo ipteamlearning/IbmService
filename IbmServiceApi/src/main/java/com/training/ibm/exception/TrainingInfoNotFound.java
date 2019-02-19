bd94c608bff4145968cce24042556118f65af82a
saketkashyap <sakkashy@in.ibm.com>
2019-02-19 13:16:58 +0530

    pushing new folders in

--- IbmServiceApi/src/main/java/com/training/ibm/exception/TrainingInfoNotFound.java
+++ IbmServiceApi/src/main/java/com/training/ibm/exception/TrainingInfoNotFound.java
@@ -1,7 +1,16 @@
 package com.training.ibm.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class TrainingInfoNotFound extends RuntimeException{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public TrainingInfoNotFound(String message)
 	{
 		super(message);
